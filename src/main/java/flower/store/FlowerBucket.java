28cb1b59ff8fda72b94fc39949d86c75a763a9d8
NastiaPlaskonis <116552566+NastiaPlaskonis@users.noreply.github.com>
2023-10-12 22:47:27 +0300

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -3,14 +3,9 @@ package flower.store;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
 
 
 public class FlowerBucket extends Item {
-
-    @Setter @Getter
-
     private List<FlowerPack> flowerPacks = new ArrayList<>();
 
     public void add(FlowerPack flowerPack) {
@@ -25,24 +20,3 @@ public class FlowerBucket extends Item {
         return price;
     }
 }
-
-// package flower.store;
-
-// import java.util.ArrayList;
-// import java.util.List;
-
-// public class FlowerBucket extends Item {
-//     private List<FlowerPack> flowerPacks = new ArrayList<>();
-
-//     public void add(FlowerPack flowerPack) {
-//         flowerPacks.add(flowerPack);
-//     }
-
-//     public double getPrice() {
-//         double price = 0;
-//         for (FlowerPack flowerPack : flowerPacks) {
-//             price += flowerPack.getPrice();
-//         }
-//         return price;
-//     }
-// }
